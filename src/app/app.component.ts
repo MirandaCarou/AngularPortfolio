17981dfbf6df36523ba20d5f9a8da0a313df25fd
Miranda <71145273+MirandaCarou@users.noreply.github.com>
2024-04-12 01:31:43 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 import { EnterPageComponent } from './enter-page/enter-page.component';
-import { RouterOutlet } from '@angular/router';
+import { NavigationStart, Router, RouterOutlet } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,19 @@ import { RouterOutlet } from '@angular/router';
   imports: [EnterPageComponent, RouterOutlet],
 })
 export class AppComponent{
+  constructor(private router: Router, private location: Location) {}
 
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        sessionStorage.setItem('previousUrl', this.location.path());
+      }
+    });
+
+    const previousUrl = sessionStorage.getItem('previousUrl');
+    if (previousUrl) {
+      this.router.navigateByUrl(previousUrl);
+      sessionStorage.removeItem('previousUrl');
+    }
+  }
 }
